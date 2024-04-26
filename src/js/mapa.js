048d96718443fa0c7a65a013ded654f246f81659
Lisandro <lisansierra@outlook.com>
2024-04-25 23:52:52 -0400

    Generando mapa en la vista

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -1,1 +1,12 @@
-alert('soy el mapaaa')
+(function() {
+    const lat = 7.7182794;
+    const lng = -67.7222129;
+    const mapa = L.map('mapa').setView([lat, lng ], 16);
+    
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mapa);
+
+
+})()
