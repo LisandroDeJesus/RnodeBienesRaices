146d819c7dcc02a704310281ec36fa43fe39607e
Lisandro <lisansierra@outlook.com>
2024-05-04 08:58:04 -0400

    Aniamndo nuestro mapa

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -1,12 +1,29 @@
 (function() {
-    const lat = 51.9279573;
-    const lng = 4.4084277;
+    const lat = 43.7181228;
+    const lng = -79.5428666;
     const mapa = L.map('mapa').setView([lat, lng ], 16);
+    let marker;
     
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapa);
+
+    //El Pin:
+     marker = new L.marker([lat, lng], {
+        draggable: true,
+        autoPan: true
+
+    })
+    .addTo(mapa)
+    
+
+    //Detectar el Movimiento del pin:
+    marker.on('moveend', function(e){
+    marker = e.target
+    const posicion = marker.getLatLng();
+    mapa.panTo(new L.LatLng(posicion.lat , posicion.lng))
+    })
 
 
 })()
