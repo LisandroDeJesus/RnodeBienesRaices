8ffd426c9fdd94610df262e5fba9c12ed4058079
Lisandro <lisansierra@outlook.com>
2024-06-07 12:00:34 -0400

    Validaciones Completadas

--- controller/propiedadesController.js
+++ controller/propiedadesController.js
@@ -2,48 +2,55 @@ import { validationResult } from "express-validator";
 import Precio from "../models/Precio.js";
 import Categoria from "../models/Categoria.js";
 
-const obtenerCategoriasYPrecios = async () => {
-  const [categorias, precios] = await Promise.all([
-    Categoria.findAll(),
-    Precio.findAll()
-  ]);
-  return { categorias, precios };
-};
+const admin = (req, res) =>{
+    res.render('propiedades/admin',{
+        pagina: 'Mis Propiedades',
+        barra:true 
+    })
+}
 
-const admin = (req, res) => {
-  res.render("propiedades/admin", {
-    pagina: "mis propiedades",
-    barra: true
-  });
-};
-
+// Formulario para crear una nueva propiedad
 const crear = async (req, res) => {
-  const { categorias, precios } = await obtenerCategoriasYPrecios();
-  res.render("propiedades/crear", {
-    pagina: "Ingrese su Propiedad",
-    barra: true,
-    csrfToken: req.csrfToken(),
-    categorias,
-    precios
-  });
-};
+    // Consultar Modelo de Precio y Categorias
+    const [categorias, precios] = await Promise.all([
+        Categoria.findAll(),
+        Precio.findAll()
+    ])
+
+    res.render('propiedades/crear', {
+        pagina: 'Crear Propiedad',
+        csrfToken: req.csrfToken(),
+        categorias,
+        precios,
+        datos: {}
+    })
+}
 
 const guardar = async (req, res) => {
-  const resultado = validationResult(req);
-  if (!resultado.isEmpty()) {
-    const { categorias, precios } = await obtenerCategoriasYPrecios();
-    return res.render("propiedades/crear", {
-      pagina: "Ingrese su Propiedad",
-      barra: true,
-      categorias,
-      precios,
-      errores: resultado.array()
-    });
-  } else {
-    // Manejar el caso en el que resultado.isEmpty() es true
-    // Por ejemplo, redirigir a una página de éxito o mostrar un mensaje de éxito
-    res.redirect("/propiedades/admin");
-  }
-};
 
-export { admin, crear, guardar };
+    // Validación
+    let resultado = validationResult(req)
+
+    if(!resultado.isEmpty()) {
+
+        // Consultar Modelo de Precio y Categorias
+        const [categorias, precios] = await Promise.all([
+            Categoria.findAll(),
+            Precio.findAll()
+        ])
+
+        return res.render('propiedades/crear', {
+            pagina: 'Crear Propiedad',
+            csrfToken: req.csrfToken(),
+            categorias,
+            precios, 
+            errores: resultado.array(),
+            datos: req.body
+        })
+    }
+
+}
+
+export{ admin, 
+    crear, 
+    guardar }
--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -13,6 +13,15 @@ router.get('/propiedades/crear', crear);
 
 router.post('/propiedades/crear',
   body('titulo').notEmpty().withMessage('El título del anuncio es obligatorio'),
+  body('descripcion')
+  .notEmpty().withMessage('La Descripción no puede ir vacia')
+  .isLength({ max: 200 }).withMessage('La Descripción es muy larga'),
+body('categoria').isNumeric().withMessage('Selecciona una categoría'),
+body('precio').isNumeric().withMessage('Selecciona un rango de Precios'),
+body('habitaciones').isNumeric().withMessage('Selecciona la Cantidad de Habitaciones'),
+body('estacionamiento').isNumeric().withMessage('Selecciona la Cantidad de Estacionamientos'),
+body('wc').isNumeric().withMessage('Selecciona la Cantidad de Baños'),
+body('lat').notEmpty().withMessage('Ubica la Propiedad en el Mapa'),
   guardar
 );
 
